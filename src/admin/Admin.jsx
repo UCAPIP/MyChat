5c2f44f6a5b8da8fc812544f8e2fffe1161df1b7
UCAPIP <ucapip@gmail.com>
2024-08-06 15:20:48 +0300

    Стилизовал Admin.jsx попытался в BEM...

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setNewUser } from "../redux/userSlice";
 import { setNewChat } from "../redux/messageSlice";
-
+import "./Admin.css";
 
 
 
@@ -52,16 +52,22 @@ const Admin = () => {
 
 
     return(
-        <div>
-            <h2>Создать пользователя</h2>
-            <form>
-                <label>Имя</label>
-                <input type="text" onChange={nameChange} value={name}/>
-                <label>Логин</label>
-                <input type="text" onChange={loginChange} value={login}/>
-                <label>Пароль</label>
-                <input type="text" onChange={passwordChange} value={password}/>
-                <input type="button" value="Создать" onClick={addUser}/>
+        <div className="admin">
+            <h2 className="admin__header">Создать пользователя</h2>
+            <form className="admin__form">
+                <div className="admin__form-input">
+                    <label>Имя</label>
+                    <input type="text" onChange={nameChange} value={name}/>
+                </div>
+                <div className="admin__form-input">
+                    <label>Логин</label>
+                    <input type="text" onChange={loginChange} value={login}/>
+                </div>
+                <div className="admin__form-input">
+                    <label>Пароль</label>
+                    <input type="text" onChange={passwordChange} value={password}/>
+                </div>
+                <input className="admin__form-btn" type="button" value="Создать" onClick={addUser}/>
             </form>
         </div>
     )
